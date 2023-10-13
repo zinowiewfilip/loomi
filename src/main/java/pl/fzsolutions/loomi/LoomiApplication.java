cce0186baae21a486dd94e7ee518361efc64c9f8
fzino <fzinowiew@gmail.com>
2023-10-13 15:55:19 +0200

    initial commit

---
+++ src/main/java/pl/fzsolutions/loomi/LoomiApplication.java
@@ -0,0 +1,13 @@
+package pl.fzsolutions.loomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoomiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/fzsolutions/loomi/LoomiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fzsolutions.loomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoomiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
